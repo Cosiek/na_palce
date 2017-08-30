29db9649b7e497a90df9d35474230deefbef3274
cosiek <witold.majchrzak@21s.pl>
2017-08-30 15:29:14 +0200

    Keep current valves state in an unordered map

--- na_palce/cpp/simplegamehandler.cpp
+++ na_palce/cpp/simplegamehandler.cpp
@@ -1,14 +1,20 @@
 #include "simplegamehandler.h"
+#include <unordered_map>
+
+
+std::unordered_map<std::string, bool> pressed;
 
 SimpleGameHandler::SimpleGameHandler(QObject *parent) : QObject(parent)
 {}
 
 QString SimpleGameHandler::key_pressed(QString key_name)
 {
+    pressed[key_name.toUtf8().constData()] = true;
     return key_name + " pressed (C++)";
 }
 
 QString SimpleGameHandler::key_released(QString key_name)
 {
+    pressed[key_name.toUtf8().constData()] = false;
     return key_name + " released (C++)";
 }
