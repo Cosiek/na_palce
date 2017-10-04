7fdc487c85e1c52f9894662207788d0889868327
cosiek <witold.majchrzak@21s.pl>
2017-10-05 00:43:00 +0200

    Handle key pres logic

--- na_palce/cpp/simplegamehandler.cpp
+++ na_palce/cpp/simplegamehandler.cpp
@@ -35,20 +35,51 @@ void SimpleGameHandler::set_state(){
     current_notes.push_back(get_random_note());
 }
 
+void changeNote(){
+    current_notes.pop_front();
+    current_notes.push_back(get_random_note());
+}
+
 SimpleGameHandler::SimpleGameHandler(QObject *parent) : QObject(parent)
 {}
 
 QString SimpleGameHandler::key_pressed(QString key_name)
 {
-    pressed[key_name.toUtf8().constData()] = true;
-    for(auto iter=pressed.begin(); iter!=pressed.end(); iter++){
-        auto cur = iter->first;
+    std::string key = key_name.toUtf8().constData();
+    // set keys state
+    pressed[key] = true;
+    // compare current keys state with what current note type state
+    if (current_notes.front().match(pressed)){
+        // if correct, get new random note,
+        changeNote();
+    // if not, check if step in right dirrection
+    } else if (!current_notes.front().isMistake(key, true)){
+        // if change not in right direction, mark error
+        qDebug() << "BŁĄD " << key.c_str();
+        // TODO increase mistakes counter
     }
     return key_name + " pressed (C++)";
+
+    //for(auto iter=pressed.begin(); iter!=pressed.end(); iter++){
+    //    auto cur = iter->first;
+    //    qDebug() << cur.c_str() << " = " << pressed[cur];
+    //}
 }
 
 QString SimpleGameHandler::key_released(QString key_name)
 {
-    pressed[key_name.toUtf8().constData()] = false;
+    std::string key = key_name.toUtf8().constData();
+    // set keys state
+    pressed[key] = false;
+    // compare current keys state with what current note type state
+    if (current_notes.front().match(pressed)){
+        // if correct, get new random note,
+        changeNote();
+    // if not, check if step in right dirrection
+    } else if (!current_notes.front().isMistake(key, false)){
+        // if change not in right direction, mark error
+        qDebug() << "BŁĄD " << key.c_str();
+        // TODO increase mistakes counter
+    }
     return key_name + " released (C++) ";
 }
