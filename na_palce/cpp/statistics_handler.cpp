ad2cc19e2eefe37cd3b1b4a0937946044d647cde
cosiek <witold.majchrzak@21s.pl>
2018-08-13 00:05:55 +0200

    Count correct played notes

--- na_palce/cpp/statistics_handler.cpp
+++ na_palce/cpp/statistics_handler.cpp
@@ -19,9 +19,11 @@ StatisticsHandler::StatisticsHandler(QObject *parent) : QObject(parent)
 
 void StatisticsHandler::reset(){
     this->total_mistakes = 0;
+    this->total_played = 0;
 
     for(auto const &ent : this->notesStats) {
         notesStats[ent.first].mistakesCount = 0;
+        notesStats[ent.first].playedCount = 0;
     }
 }
 
@@ -29,12 +31,14 @@ void StatisticsHandler::reset(){
 QString StatisticsHandler::get_stats(){
     QJsonObject statsObject;
     statsObject["total_mistakes"] = this->total_mistakes;
+    statsObject["total_played"] = this->total_played;
 
     QJsonObject notesJson;
-    NoteStatistic stat;
     for(auto const& ent : this->notesStats) {
-        stat = ent.second;
-        notesJson[ent.first] = stat.mistakesCount;
+        QJsonObject noteJson;
+        noteJson["mistakes_count"] = ent.second.mistakesCount;
+        noteJson["played_count"] = ent.second.playedCount;
+        notesJson[ent.first] = noteJson;
     }
     statsObject["notes"] = notesJson;
 
@@ -46,4 +50,10 @@ QString StatisticsHandler::get_stats(){
 void StatisticsHandler::countMistake(Note note){
     this->total_mistakes++;
     this->notesStats[note.noteType.getId()].mistakesCount++;
+}
+
+
+void StatisticsHandler::countCorrect(Note note){
+    this->total_played++;
+    this->notesStats[note.noteType.getId()].playedCount++;
 }
